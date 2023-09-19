b753fb3cf7c0b4a0ccee541cc83a8c7af0c38e7a
Fagorym <oleg-veber@mail.ru>
2023-09-19 18:12:54 +0700

    Initial commit

---
+++ src/main/java/ru/nsu/fit/publify/publify/PublifyApplication.java
@@ -0,0 +1,13 @@
+package ru.nsu.fit.publify.publify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PublifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/nsu/fit/publify/publify/PublifyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nsu.fit.publify.publify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PublifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
