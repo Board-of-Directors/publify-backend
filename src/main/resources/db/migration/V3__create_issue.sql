0b1c8a21d7f35e3d5236437a3fa1b04342e6160f
Fagor <oleg-veber@mail.ru>
2023-12-02 13:41:54 +0700

    Create issue migration

---
+++ src/main/resources/db/migration/V3__create_issue.sql
@@ -0,0 +1,9 @@
+create table issue(
+    id bigserial primary key ,
+    name varchar,
+    description varchar,
+    number int,
+    release_date date,
+    cover varchar,
+    journal_id bigint references journal(id)
+)
