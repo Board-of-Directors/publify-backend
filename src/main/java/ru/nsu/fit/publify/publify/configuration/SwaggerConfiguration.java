76006e878f682cc1357a5a51948c7a2a88cb05d4
Fagor <oleg-veber@mail.ru>
2023-12-02 17:03:56 +0700

    add swagger

---
+++ src/main/java/ru/nsu/fit/publify/publify/configuration/SwaggerConfiguration.java
@@ -0,0 +1,22 @@
+package ru.nsu.fit.publify.publify.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class SwaggerConfiguration {
+    @Value("${swagger.baseUrl}")
+    private String baseUrl;
+
+    @Bean
+    public OpenAPI customOpenApi() {
+        Server server = new Server();
+        server.setUrl(baseUrl);
+        return new OpenAPI().servers(List.of(server));
+    }
+}
