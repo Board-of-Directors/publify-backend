9624d9a3db0f01a374208a97fb63ebec9db11ba7
Fagor <oleg-veber@mail.ru>
2023-12-06 22:42:45 +0700

    Fix file creation

--- src/main/java/ru/nsu/fit/publify/publify/service/ImageServiceImpl.java
+++ src/main/java/ru/nsu/fit/publify/publify/service/ImageServiceImpl.java
@@ -1,6 +1,7 @@
 package ru.nsu.fit.publify.publify.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
@@ -12,12 +13,15 @@ import java.io.OutputStream;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class ImageServiceImpl implements ImageService {
     @Override
     public String save(byte[] coverFile, String journalTitle, Integer number) {
         String filePath = "/images/" + journalTitle + "_" + number;
-        File file = new File(filePath);
+        File file = new File(filePath.replace(" ", ""));
         try (OutputStream os = new FileOutputStream(file)) {
+            boolean success = file.createNewFile();
+            log.info("Creation file was success = {}", success);
             os.write(coverFile);
         } catch (IOException ex) {
             throw new RuntimeException(ex.getMessage());
